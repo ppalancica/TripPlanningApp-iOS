1f4b1281c0e96a127dafd20176f3e7904d15c90c
System Administrator <root@Pavels-MacBook-Pro.local>
2022-07-03 22:31:43 +0300

    First commit. Created iOS App project in Xcode

---
+++ TripPlanningApp-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripPlanningApp-iOS
+//
+//  Created by Pavel Palancica on 7/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
